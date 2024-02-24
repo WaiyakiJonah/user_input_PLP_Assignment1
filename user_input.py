7dda999d87fe3ae507706f400426ba68e349fd93
WaiyakiJonah <124672798+WaiyakiJonah@users.noreply.github.com>
2024-02-24 16:17:10 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+#Power Learn Project Assignment 1
+name=input('Please Enter Your Name')
+age=input('Please Enter Your Age')
+location=input('Please Enter Your Location')
+print('Hello',name,'you are' ,age, 'years old and live in',location,'.')
